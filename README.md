65661cef829db497daa7f282f6ce2179f02c5c25
LouisBillaut <louisbillaut2000@gmail.com>
2021-01-11 10:39:59 +0100

    beta version commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# IGM emargement auto-clicker
+This bot allows automatic sign in on the "emargement" channel of IGM server.  
+This is against Discord TOS, to use at your own risks.
+
+## DOWLOADS
+To use this bot, you must download:  
+• python 3+  
+• Chrome  
+• chromedriver with a compatible version with you Chrome version. https://chromedriver.chromium.org/  
+  
+Install this Python module:  
+• `pip install selenium`
+
+## CONFIGURATION
+config.txt is the configuration file of the bot.  
+You must write on the first line, the absolute path to the chromedriver that you download.
+
+## RUN
+`python3 autoclicker.py`  
+This will open a chrome window, just fill the connection form and let the bot run. You need to keep window opened.
---
+++ autoclicker.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import TimeoutException
+from random import randint
+from time import sleep
+
+def init():
+    """
+    Return a List of all args in settings.txt file
+    """
+    args = []
+    with open("settings.txt", "r") as reader:
+        for line in reader:
+            args.append(line)
+    return args
+
+
+def main():
+    args = init()
+    chromedriver_location = args[0]
+    driver = webdriver.Chrome(chromedriver_location)
+    #this link is the discord channel "emargement" of IGM server
+    driver.get("https://discord.com/channels/688401448131624974/773863921206558761")
+
+    while(True):
+        try:
+            wait = WebDriverWait(driver, 86400).until(ec.element_to_be_clickable((By.XPATH, "//div[@class='reactionInner-15NvIl'][@aria-label='🙋, appuie pour réagir'][@aria-pressed='false'][@role='button']")))
+            items = driver.find_elements_by_xpath("//div[@class='reactionInner-15NvIl'][@aria-label='🙋, appuie pour réagir'][@aria-pressed='false'][@role='button']")
+            for i in items:
+                #timer to be hide as a bot, can be removed if you want to be the first to sign
+                sleep(randint(1,5))
+                i.click()
+        except (NoSuchElementException, ElementNotVisibleException):
+            print("react button not found, check for the channel")
+            quit()
+        except TimeoutException:
+            print("no call message made in 24h")
+            quit()
+
+if __name__ == '__main__':
+    main()
