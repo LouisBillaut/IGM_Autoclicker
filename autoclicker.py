a2f47b33cb344ec59cb03f172646323e2de540aa
LouisBillaut <louisbillaut2000@gmail.com>
2021-01-11 10:44:58 +0100

    remove unused imports

--- autoclicker.py
+++ autoclicker.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
